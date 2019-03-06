d7d6d0a22207ee6931182fce8d063a4e8651b9c6
Kim Streicher <kim.streicher@allata.com>
2019-03-06 12:09:55 -0700

    reset password form, added in routing components too

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
+//Modules
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [
+//Components
+import { ResetPasswordComponent } from "./views/reset-password/reset-password.component";
 
-]
+const routes: Routes = [
+  { path: "ResetPassword", component: ResetPasswordComponent }
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+//Modules
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { SharedModule } from "./shared/SharedModule";
+import { AppRoutingModule } from "./app-routing.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './views/home-page/home-page.component';
-import { LogOutComponent } from './views/log-out/log-out.component';
+//Components
+import { AppComponent } from "./app.component";
+import { HomePageComponent } from "./views/home-page/home-page.component";
+import { LogOutComponent } from "./views/log-out/log-out.component";
+import { ResetPasswordComponent } from "./views/reset-password/reset-password.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    LogOutComponent
+    LogOutComponent,
+    ResetPasswordComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, SharedModule, AppRoutingModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
