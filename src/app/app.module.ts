32b6b9254276e2332d6a8d572db165a02cd7fef8
Malik Whitfield <malikwhitfield29@gmail.com>
2019-03-06 11:29:44 -0700

    yu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginPageComponent } from './views/login-page/login-page.component';
-=======
-import { HomePageComponent } from './views/home-page/home-page.component';
->>>>>>> development
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginPageComponent
-=======
-    HomePageComponent
->>>>>>> development
   ],
   imports: [
     BrowserModule
