8c06b7e200c545fadaeaeeaf60e0085a4e451dc5
Malik Whitfield <malikwhitfield29@gmail.com>
2019-03-06 14:48:27 -0700

    doom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,17 @@ import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./views/home-page/home-page.component";
 import { LogOutComponent } from "./views/log-out/log-out.component";
 import { ResetPasswordComponent } from "./views/reset-password/reset-password.component";
+import { PersonalInfoFormComponent } from './components/personal-info-form/personal-info-form.component';
+import { Error404Component } from './views/error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     LogOutComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    PersonalInfoFormComponent,
+    Error404Component
   ],
   imports: [BrowserModule, SharedModule, AppRoutingModule, FlexLayoutModule],
   providers: [],
