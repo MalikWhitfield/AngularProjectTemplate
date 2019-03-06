3b092efc5071739bdc609a34f284908a7a86ad0f
Kim Streicher <kim.streicher@allata.com>
2019-03-06 14:14:53 -0700

    404 error page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomePageComponent } from "./views/home-page/home-page.component";
 import { LogOutComponent } from "./views/log-out/log-out.component";
 import { ResetPasswordComponent } from "./views/reset-password/reset-password.component";
 import { PersonalInfoFormComponent } from './components/personal-info-form/personal-info-form.component';
+import { Error404Component } from './views/error404/error404.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { PersonalInfoFormComponent } from './components/personal-info-form/perso
     HomePageComponent,
     LogOutComponent,
     ResetPasswordComponent,
-    PersonalInfoFormComponent
+    PersonalInfoFormComponent,
+    Error404Component
   ],
   imports: [BrowserModule, SharedModule, AppRoutingModule, FlexLayoutModule],
   providers: [],
