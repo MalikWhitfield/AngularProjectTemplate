222fc25ed869e85d19c546c10a5b27816a1ea69a
Malik Whitfield <malikwhitfield29@gmail.com>
2019-03-06 14:19:37 -0700

    login function

--- src/app/views/login-page/login-page.component.ts
+++ src/app/views/login-page/login-page.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  user = {
+    username: '',
+    password: ''
+  }
+
+  login(){
+    console.log('Success. Logging {{user.username}} in...')
+    return true;
+  }
+
   constructor() { }
 
   ngOnInit() {
