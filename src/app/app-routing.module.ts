1cc8eaa4b6b19b35e1ce58823dd01bd649f700d0
Kim Streicher <kim.streicher@allata.com>
2019-03-07 11:24:58 -0700

    Removed extra reset password route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 //Modules
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { LoginPageComponent } from './views/login-page/login-page.component';
+import { LoginPageComponent } from "./views/login-page/login-page.component";
 import { ResetPasswordComponent } from "./views/reset-password/reset-password.component";
 import { PersonalInfoFormComponent } from "./components/personal-info-form/personal-info-form.component";
 import { Error404Component } from "./views/error404/error404.component";
@@ -10,13 +10,11 @@ const routes: Routes = [
   { path: "ResetPassword", component: ResetPasswordComponent },
   { path: "PersonalInfoForm", component: PersonalInfoFormComponent },
   { path: "Error404", component: Error404Component },
-  { path: 'Login', component: LoginPageComponent },
-  { path: "ResetPassword", component: ResetPasswordComponent }
-]
-
+  { path: "Login", component: LoginPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
